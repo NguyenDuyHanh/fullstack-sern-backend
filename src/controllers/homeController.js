7742862e639dc68852eabf32a3eb1e2e1a1dd8e7
NguyenDuyhanh <hanh27052004@gmail.com>
2025-07-25 13:18:59 +0700

    feat: kết nối MySQL bằng Sequelize và render dữ liệu ra view homepage

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,6 +1,13 @@
-
-let getHomePage = (req, res) => {
-    return res.render('homepage');
+import db from "../models/index.js";
+let getHomePage = async (req, res) => {
+    try {
+        const data = await db.User.findAll();
+        return res.render('homepage', {
+            data: JSON.stringify(data)
+        });
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 let getAboutPage = (req, res) => {
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import configViewEngine from "./config/viewEngine.js";
 import initWebRoutes from "./route/web.js";
+import connectDB from "./config/connectDB.js";
 
 let app = express();
 
@@ -12,6 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 configViewEngine(app);
 initWebRoutes(app);
+connectDB();
 
 dotenv.config();
 let port = process.env.PORT;
